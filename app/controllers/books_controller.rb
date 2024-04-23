4df7634142a1d82fa29dfc5ebb373c56f36c4b7d
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-04-23 14:34:24 +0000

    Bootstrap導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,15 @@
 class BooksController < ApplicationController
+  
   def new
     @book = Book.new
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def index
+    @books = Book.all 
   end
   
    # 投稿データの保存
@@ -8,7 +17,7 @@ class BooksController < ApplicationController
      @book = Book.new(book_params)
      @book.user_id = current_user.id
      @book.save
-    # redirect_to post_images_path
+     redirect_to '/books'
   end
   
    private
@@ -16,4 +25,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :image, :body )
   end
+  
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ApplicationRecord
+  validates :title, :body, presence: true
+  # ... other validations
+  has_one_attached :image  # Paperclip or Shrineなどのライブラリを使用
+  # ... other methods
+  
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end
