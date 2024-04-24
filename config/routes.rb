9fafe2388c1cbe1c690bd5043113860045a0de9c
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-04-24 13:08:17 +0000

    Homesページ完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   resources :books, only: [:new, :index, :show]
+  get 'homes/about' => 'homes#about'
 end
