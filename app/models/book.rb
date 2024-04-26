6b7709e968d54b1c60aab11cc1a95f67b6a5fa0b
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-04-26 12:01:21 +0000

    ヘッダー完成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,21 +1,9 @@
 class Book < ApplicationRecord
-  validates :title, :body, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
   # ... other validations
   # has_one_attached :image  # Paperclip or Shrineなどのライブラリを使用
   # ... other methods
   belongs_to :user
-  
-  has_one_attached :image
-
-  def self.default_image
-    Rails.root.join('app/assets/images/no_image.jpg')
-  end
-
-  def get_image
-    image ||= Book.default_image # image が nil ならば Book.default_image を代入
-    # ... (既存の処理)
-  end
-  
-  
  
 end
